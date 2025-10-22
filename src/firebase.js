95214d4ab0b9fd5882290e0bd11b7cc2cde79166
Kevalarmano <keval4ram@gmail.com>
2025-10-22 19:21:59 +0000

    react router and main.jsx edit

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// ✅ REPLACE everything inside {} with your actual Firebase config from Step 3 in Firebase Console
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
